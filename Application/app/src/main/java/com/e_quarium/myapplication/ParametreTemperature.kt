71e9ce892c6211c1ac546c9ce2195f2825d18c50
LeoThinnes <63150113+LeoThinnes@users.noreply.github.com>
2021-02-10 17:24:34 +0100

    lecture bd

--- Application/app/src/main/java/com/e_quarium/myapplication/ParametreTemperature.kt
+++ Application/app/src/main/java/com/e_quarium/myapplication/ParametreTemperature.kt
@@ -1,22 +1,25 @@
 package com.e_quarium.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class ParametreTemperature : AppCompatActivity() {
 
-
+    var database = FirebaseDatabase.getInstance()
+    var myRef = database.getReference("test2")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_parametre_temperature)
-        var database = FirebaseDatabase.getInstance()
-        var myRef = database.getReference("test")
 
-        myRef.setValue(22)
 
         supportActionBar?.hide()
 
@@ -28,10 +31,29 @@ class ParametreTemperature : AppCompatActivity() {
         val editText:String? = findViewById<EditText>(R.id.editTextNumber).toString()
 
         val btnValider = findViewById<Button>(R.id.btnValider)
+        btnValider.setOnClickListener{
 
 
+            myRef.setValue(27)
+        }
 
-       
+        val txtView = findViewById<TextView>(R.id.textView4)
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(temperature: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = temperature.getValue().toString()
+                val temperature = findViewById<TextView>(R.id.temp)
+                txtView.setText(value)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("TAG", "Failed to read value.", error.toException())
+            }
+        })
+
+
 
     }
 
--- Application/app/src/main/java/com/e_quarium/myapplication/affichage_temperature.kt
+++ Application/app/src/main/java/com/e_quarium/myapplication/affichage_temperature.kt
@@ -6,12 +6,15 @@ import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-
+import com.google.firebase.database.ValueEventListener
 
 
 class affichage_temperature : AppCompatActivity() {
-
+    var database = FirebaseDatabase.getInstance()
+    var myRef = database.getReference("temp")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,11 +35,22 @@ class affichage_temperature : AppCompatActivity() {
         }
 
         val temperature = findViewById<TextView>(R.id.temp)
-        temperature.setText("26")
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(data: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = data.getValue().toString()
+                temperature.setText(value)
+            }
 
-
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("TAG", "Failed to read value.", error.toException())
+            }
+        })
 
     }
+
 
 
 }
